bfc83512a7ab5d38f170c0c5a2b77f48f8723d23
clove-424 <cjlove2005@outlook.com>
2025-04-29 13:47:48 -0400

    can now create threads
    unfortunately, when leaving or refreshing the page, the thread is deleted.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function hamburgerMenu() {
   var x = document.getElementById("myNavigation");
   if (x.className == "navigation") {
     x.className += " responsive";
@@ -6,3 +6,24 @@ function myFunction() {
     x.className = "navigation";
   }
 }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#comment-form");
+    const list = document.querySelector("#content");
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const description = document.querySelector("#myComment").value;
+      const thread = document.createElement("div");
+      thread.className = "threads";
+      const d = new Date();
+      var day = d.getDate() + " " + d.toLocaleString('default', { month: 'long' }) + " " + d.getFullYear();
+      thread.innerHTML = `\t
+      <small>${day}</small>
+      <p>\t${description}</p>
+      <a href="#">\tReply</a>
+      `;
+      list.appendChild(thread);
+      form.reset();
+    });
+  });
+
